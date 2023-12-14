0e83ff026e7bff2b47176d0af3402c5ba98eaf62
AdamLahovskyi <ipz221_lav@student.ztu.edu.ua>
2023-12-14 16:11:24 +0200

    started working on cart feature

---
+++ MarketPlace/cart/cart.py
@@ -0,0 +1,38 @@
+from item.models import Item
+
+class Cart():
+    def __init__(self, request):
+        self.session = request.session
+
+        cart = self.session.get('session_key')
+
+        if 'session_key' not in request.session:
+            cart = self.session['session_key'] = {}
+
+        self.cart = cart
+
+    def add(self, item, quantity):
+        product_id = str(item.id)
+        product_qty = str(quantity)
+        if product_id in self.cart:
+            pass
+        else:
+            #self.cart[product_id] = {'price': str(item.price)}
+            self.cart[product_id] = int(product_qty)
+
+        self.session.modified = True
+
+    def __len__(self):
+        return len(self.cart)
+
+    def get_prods(self):
+        product_ids = self.cart.keys()
+        items = Item.objects.filter(id__in=product_ids)
+
+        return items
+
+    def get_quants(self):
+        quantities = self.cart
+
+        return quantities
+
---
+++ MarketPlace/cart/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from item.models import Item
+from django.http import JsonResponse
+
+def cart_summary(request):
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    return render(request, "cart_summary.html", {"cart_products":cart_products, "quantities": quantities})
+def cart_add(request):
+    #get the cart
+    cart = Cart(request)
+    #POST test
+    if request.POST.get('action') == 'post':
+        product_id = request.POST.get('product_id')
+        product_qty = request.POST.get('product_qty')
+
+        #look for item in db
+        item = get_object_or_404(Item, id=product_id)
+        #save to sess
+        cart.add(item=item, quantity=product_qty)
+
+        cart_quantity = cart.__len__()
+
+        response = JsonResponse({'qty': cart_quantity})
+        return response
+def cart_delete(request):
+    pass
+def cart_update(request):
+    pass

6fe5712d3ba7a2669f7533d2b7f93d7ea07b7ecb
AdamLahovskyi <ipz221_lav@student.ztu.edu.ua>
2023-12-15 19:28:10 +0200

    update button on cart page is now done and working perfectly

--- MarketPlace/cart/cart.py
+++ MarketPlace/cart/cart.py
@@ -36,3 +36,15 @@ class Cart():
 
         return quantities
 
+    def update(self, product, quantity):
+        product_id =str(product)
+        product_qty = int(quantity)
+
+        outcart = self.cart
+
+        outcart[product_id] = product_qty
+
+        self.session.modified = True
+
+        thing = self.cart
+        return thing
--- MarketPlace/cart/views.py
+++ MarketPlace/cart/views.py
@@ -25,7 +25,19 @@ def cart_add(request):
 
         response = JsonResponse({'qty': cart_quantity})
         return response
+
+
 def cart_delete(request):
     pass
+
+
 def cart_update(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = request.POST.get('product_id')
+        product_qty = request.POST.get('product_qty')
+
+        cart.update(product=product_id, quantity = product_qty)
+        response = JsonResponse({'qty':product_qty})
+
+        return response
